cd09f9994347f2dcc830dc3afc66eda11f49c99f
Hellden <r.chapotin@gmail.com>
2019-09-14 19:32:11 +0200

    add ideas vuex

---
+++ src/components/Ideas.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Ideas</h2>
+    <ul>
+      <li v-for="idea in allIdeas" :key="idea.id">{{ idea.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    allIdeas() {
+      return this.$store.getters("allIdeas");
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped></style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    ideas: {}
+  },
+  mutations: {
+    CREATE_IDEAS(state, payload) {
+      const id = Date.now();
+      state.ideas[id] = payload;
+      state.ideas = { ...state.ideas };
+    }
+  },
+  actions: {
+    createIdea(context, payload) {
+      context.commit("CREATE_IDEAS", payload);
+    }
+  },
+  getters: {
+    allIdeas(state) {
+      const allIdeas = Object.keys(state.ideas).map(key => state.ideas[key]);
+      return allIdeas;
+    }
+  }
 });
